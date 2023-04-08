186a45be198fc6d0e73792c6cca6c5a41ce4ab60
Alex Maldonado Noh <alex.maldonado2919@gmail.com>
2023-04-08 13:24:56 -0500

    exercise 11.20 my own pipeline on bloglistapp

--- api/app.js
+++ api/app.js
@@ -34,6 +34,10 @@ app.use('/api/blogs',blogsRouter);
 app.use('/api/users',usersRouter);
 app.use('/api/login',loginRouter);
 
+app.get('/health',(_req,res) => {
+    res.send('ok').end();
+});
+
 if(process.env.NODE_ENV === 'test'){
     const testingRouter = require('./controllers/testingRouter');
     app.use('/api/testing',testingRouter);
